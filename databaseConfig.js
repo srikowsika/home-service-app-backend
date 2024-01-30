acc380a4d76e8162b5dc1dc65225ae3819e9d68a
srikowsika.guna <srikowsika.guna@aspiresys.com>
2024-01-30 08:50:36 +0530

    new changes backend

--- databaseConfig.js
+++ databaseConfig.js
@@ -7,21 +7,7 @@ async function run() {
   try {
     console.log("paakj");
     database = client.db("home-service-app-database");
-    //  console.log(database
-    //  let options = {sort: { title: 1 }};
-    // Include only the `title` and `imdb` fields in each returned document
-    // projection: { _id: 0, service: 1 },}
-    // let list = await services.find({}, options);
-    // res.send(services);
     return database;
-
-    // const query = { service: 'Cleaning' };
-    // const newService = await services.insertOne(query);
-    // let options = {sort: { title: 1 },
-    // // Include only the `title` and `imdb` fields in each returned document
-    // projection: { _id: 0, service: 1 },}
-
-    // console.log(newService);
   }finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express')
 const cors = require('cors');
-const database = require('./databaseConfig.js')
-const app = express()
-const port = 4000
+const database = require('./databaseConfig.js');
+const app = express();
+const port = 4000;
+var bodyParser = require('body-parser');
+
+
 const serviceRoutes = require('./routes/services.js'),
   cityRoutes = require("./routes/city.js");
   orderRoutes = require("./routes/order.js");
 
 
-var bodyParser = require('body-parser')
+
 
 app.use(bodyParser.json({limit: '50mb'}));
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-// app.use(bodyParser.json({limit: '50mb'}));
 app.use(cors());
+
+
+
 app.use('/services',serviceRoutes);
 app.use("/city", cityRoutes);
 app.use('/order', orderRoutes);
 app.use('/', (req, res) => {
   console.log("new route")
   res.send('hello world')
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
